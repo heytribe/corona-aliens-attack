70a05713d89c54113b2ea5f4b1a27791b8087224
Nicolas BRADIER <>
2018-03-19 15:22:28 -0700

    no message

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local composer = require "composer"
 
 -- Global - Hackish way to detect the simulator...
-isSimulator = false -- system.getInfo("build") == '2017.3184'
+isSimulator = true -- system.getInfo("build") == '2017.3184'
 
 function log(string)
 	-- print('🍺 - Corona - ' .. string)
