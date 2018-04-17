d11147393ea4836a4ec5f4cde2e0c073cc081350
Nicolas BRADIER <nicolas.bradier@gmail.com>
2018-04-17 15:38:50 -0700

    Remove duplicate

--- localsounds.lua
+++ localsounds.lua
@@ -6,36 +6,6 @@ local exports = {}
 ---------------------------------------------------------------------------------
 -- Export Functions
 
-exports.load = function()
-	if system.getInfo("platform") == "android" then
-		shouldHandleSoundNatively = false
-		loadedSounds = {
-			alienKilled = audio.loadSound(soundPath("alien_killed")),
-			playerLost  = audio.loadSound(soundPath("player_lost")),
-			playerWon   = audio.loadSound(soundPath("player_won")),
-			watch       = audio.loadSound(soundPath("watch")),
-		}
-		loadedStreams = {
-			audio.loadStream(soundPath("soundtrack_0")),
-			audio.loadStream(soundPath("soundtrack_1")),
-			audio.loadStream(soundPath("soundtrack_2")),
-		}
-	else
-		shouldHandleSoundNatively = true
-		loadedSounds = {
-			alienKilled = soundPath("alien_killed"),
-			playerLost  = soundPath("player_lost"),
-			playerWon   = soundPath("player_won"),
-			watch       = soundPath("watch"),
-		}
-		loadedStreams = {
-			soundPath("soundtrack_0"),
-			soundPath("soundtrack_1"),
-			soundPath("soundtrack_2"),
-		}
-	end
-end
-
 exports.load = function(handleNatively, soundPath)
 
 	if not handleNatively then
